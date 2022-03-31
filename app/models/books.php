f47c8ed63f860593c77ec66d584718abd8bb0527
samia saeed <samsaeed738@gmail.com>
2022-03-31 14:22:50 +0300

    first upload to my e-library

---
+++ app/models/books.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+
+class Books extends Model{
+    function __construct()
+    {
+
+        parent::$tblName="books";
+    }
+}
+?>
---
+++ app/models/category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+
+
+class Category extends Model{
+    function __construct()
+    {
+
+        parent::$tblName="categories";
+    }
+}
+?>
